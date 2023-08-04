a1d4a9d0c10bfc6e3a0ec91e0a802490f97e2f3d
80sDweeb <scott.alan.williams@gmail.com>
2023-08-04 07:25:35 -0700

    Update httpvalidate.py
    updated with what I have locally

--- httpvalidate.py
+++ httpvalidate.py
@@ -2,6 +2,7 @@ import requests
 import time
 import socket
 
+
 # All the test functions
 def long_header_test(url):
     header_value = 'a' * 10000
@@ -9,45 +10,55 @@ def long_header_test(url):
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def long_request_test(url):
     params = {'param': 'a' * 10000}
     response = requests.get(url, params=params)
     print(response.status_code)
+
 
 def transfer_encoding_test(url):
     headers = {'Transfer-Encoding': 'chunked', 'Content-Length': '100'}
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
-def double_url_encoding_test(url):
+
+def double_url_encoding_test(url): # causes errors
     url = url + "%2520"  # Double URL encoded space
     response = requests.get(url)
     print(response.status_code)
+
 
 def extremely_long_http_request(url):
     params = {'param': 'a' * 60000}
     response = requests.get(url, params=params)
     print(response.status_code)
 
+
 def extremely_long_parameter(url):
     params = {'param': 'a' * 60000}
     response = requests.get(url, params=params)
     print(response.status_code)
+
 
 def http_websocket_violation_test(url):
     headers = {'Connection': 'Upgrade', 'Upgrade': 'websocket'}
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def illegal_byte_code_char_in_header_name_test(url):
     headers = {'\x00Custom-Header': 'value'}
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def illegal_byte_code_char_in_header_value_test(url):
     headers = {'Custom-Header': '\x00value'}
     response = requests.get(url, headers=headers)
     print(response.status_code)
+
+
 def illegal_byte_code_char_in_method_test(url):
     try:
         response = requests.request('\x00GET', url)
@@ -55,28 +66,33 @@ def illegal_byte_code_char_in_method_test(url):
     except Exception as e:
         print(f"Illegal Byte Code Character in Method Test Exception: {e}")
 
+
 def illegal_byte_code_char_in_param_name_test(url):
     params = {'\x00param': 'value'}
     response = requests.get(url, params=params)
     print(response.status_code)
+
 
 def illegal_byte_code_char_in_param_value_test(url):
     params = {'param': '\x00value'}
     response = requests.get(url, params=params)
     print(response.status_code)
 
+
 def illegal_byte_code_char_in_query_string_test(url):
     url = url + '?query=\x00value'
     response = requests.get(url)
     print(response.status_code)
+
 
 def illegal_byte_code_char_in_url_test(url):
     url = url + '\x00'  # append NULL byte to URL
     response = requests.get(url)
     print(response.status_code)
 
-#def illegal_chunk_size_test(url):
-    # this test may need a raw socket connection and may be server specific
+
+# def illegal_chunk_size_test(url):
+# this test may need a raw socket connection and may be server specific
 #    pass
 
 def illegal_content_length_test(url):
@@ -84,13 +100,15 @@ def illegal_content_length_test(url):
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def illegal_content_type_test(url):
     headers = {'Content-Type': '\x00'}  # NULL byte in content type
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
-#def illegal_http_version_test(url):
-    # this test may need a raw socket connection to change the HTTP version
+
+# def illegal_http_version_test(url):
+# this test may need a raw socket connection to change the HTTP version
 #    pass
 
 def illegal_host_name_test(url):
@@ -98,74 +116,88 @@ def illegal_host_name_test(url):
     response = requests.get(url)
     print(response.status_code)
 
+
 def illegal_parameter_encoding_test(url):
     params = {'param': 'value\x00'}  # Null byte in parameter value
     response = requests.get(url, params=params)
     print(response.status_code)
 
-#def illegal_response_code_test(url):
-  # this test requires a custom server that can return illegal response codes
- #   pass
+
+# def illegal_response_code_test(url):
+# this test requires a custom server that can return illegal response codes
+#   pass
 
 def illegal_url_path_encoding_test(url):
     url = url + "/%AF%AF"  # Invalid percent encoding
     response = requests.get(url)
     print(response.status_code)
 
+
 def illegal_content_encoding_test(url):
     headers = {'Content-Encoding': '\x00'}  # NULL byte in content encoding
     response = requests.get(url, headers=headers)
     print(response.status_code)
+
 
 def malformed_http_header_line_test(url):
     headers = {'X-Custom-Header': 'value\r\nX-Malformed-Header: value'}
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def malformed_json_message_test(url):
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, data='{"malformed json"}')
     print(response.status_code)
+
 
 def malformed_soap_message_test(url):
     headers = {'Content-Type': 'application/soap+xml'}
     response = requests.post(url, headers=headers, data='<soap:malformed/>')
     print(response.status_code)
 
+
 def malformed_url_test(url):
     url = url + '%'  # Append malformed percent encoding
     response = requests.get(url)
     print(response.status_code)
+
 
 def null_char_in_header_name_test(url):
     headers = {'X-Custom\x00Header': 'value'}  # NULL byte in header name
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def null_char_in_header_value_test(url):
     headers = {'X-Custom-Header': '\x00value'}  # NULL byte in header value
     response = requests.get(url, headers=headers)
     print(response.status_code)
+
 
 def null_char_in_param_name_test(url):
     params = {'\x00param': 'value'}
     response = requests.get(url, params=params)
     print(response.status_code)
 
+
 def null_char_in_param_value_test(url):
     params = {'param': '\x00value'}
     response = requests.get(url, params=params)
     print(response.status_code)
+
 
 def post_request_missing_content_type_test(url):
     headers = {'Content-Length': '100'}
     response = requests.post(url, headers=headers, data='data')  # POST request with missing content type
     print(response.status_code)
 
+
 def redundant_http_headers_test(url):
     headers = {'X-Custom-Header': 'value', 'X-Custom-Header': 'value2'}  # Redundant headers
     response = requests.get(url, headers=headers)
     print(response.status_code)
+
 
 def slow_http_from_single_source_test(url):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -176,10 +208,12 @@ def slow_http_from_single_source_test(url):
         time.sleep(1)  # Sleep for 1 second
     s.close()
 
+
 def too_many_cookies_in_a_request_test(url):
     cookies = {f"cookie{i}": "value" for i in range(101)}  # Generate 101 cookies, exceeds common limit
     response = requests.get(url, cookies=cookies)
     print(response.status_code)
+
 
 def too_many_headers_per_request_test(url):
     headers = {}
@@ -188,15 +222,18 @@ def too_many_headers_per_request_test(url):
     response = requests.get(url, headers=headers)
     print(response.status_code)
 
+
 def too_many_url_parameters_test(url):
     params = {f"param{i}": "value" for i in range(101)}  # Exceed common limit of 100 parameters
     response = requests.get(url, params=params)
     print(response.status_code)
 
+
 def url_is_above_root_directory_test(url):
     url = url + "/../"  # Attempt to access parent directory
     response = requests.get(url)
     print(response.status_code)
+
 
 def unknown_http_request_method_test(url):
     try:
@@ -204,6 +241,12 @@ def unknown_http_request_method_test(url):
         print(response.status_code)
     except Exception as e:
         print(f"Unknown HTTP Request Method Test Exception: {e}")
+
+
+def good_test(url):
+    response = requests.get(url)
+    print(response.status_code)
+
 
 def main_menu():
     print("Web Server HTTP Validation Test Menu")
@@ -232,10 +275,13 @@ def main_menu():
     print("22. Malformed JSON Message Test")
     print("23. Malformed SOAP Message Test")
     print("24. Malformed URL Test")
+    print("25. Good URL Test")
+
 
 def url_prompt():
     url = input("Enter the URL to test: ")
     return url
+
 
 def main():
     while True:
@@ -293,3 +339,9 @@ def main():
                 malformed_soap_message_test(url)
             elif choice == '24':
                 malformed_url_test(url)
+            elif choice == '25':
+                good_test(url)
+
+
+if __name__ == "__main__":
+    main()
